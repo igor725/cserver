23d978f7518dbad8b4b01d57984591c0bee16bf9
igor725 <admin@igvx.ru>
2019-08-28 15:47:42 +0300

    Whoops

--- platform.c
+++ platform.c
@@ -186,7 +186,8 @@ SOCKET Socket_Bind(const char* ip, ushort port) {
 	ssa.sin_port = htons(port);
 	ssa.sin_addr.s_addr = inet_addr(ip);
 
-	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (const void*)1, 4);
+	int val = 1;
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (const void*)val, 4);
 	if(bind(fd, (const struct sockaddr*)&ssa, sizeof(ssa)) == -1) {
 		Error_Set(ET_SYS, GetLastError());
 		return INVALID_SOCKET;
