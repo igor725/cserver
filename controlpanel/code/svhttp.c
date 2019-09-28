e3a8e360272aa35f53e89ba699702a3a3ce5e270
igor725 <admin@igvx.ru>
2019-09-28 16:50:09 +0300

    Fixes

--- controlpanel/code/svhttp.c
+++ controlpanel/code/svhttp.c
@@ -15,74 +15,73 @@ unzFile zData;
 */
 
 static const char* getReasonPhrase(int code) {
-	switch (code)
-	{
-	case 100: return "Continue";
-	case 101: return "Switching Protocols";
-	case 102: return "Processing";
-	case 103: return "Early Hints";
+	switch (code) {
+		case 100: return "Continue";
+		case 101: return "Switching Protocols";
+		case 102: return "Processing";
+		case 103: return "Early Hints";
 
-	case 200: return "OK";
-	case 201: return "Created";
-	case 202: return "Accepted";
-	case 203: return "Non-Authoritative Information";
-	case 204: return "No Content";
-	case 205: return "Reset Content";
-	case 206: return "Partial Content";
-	case 207: return "Multi-Status";
-	case 208: return "Already Reported";
-	case 226: return "IM Used";
+		case 200: return "OK";
+		case 201: return "Created";
+		case 202: return "Accepted";
+		case 203: return "Non-Authoritative Information";
+		case 204: return "No Content";
+		case 205: return "Reset Content";
+		case 206: return "Partial Content";
+		case 207: return "Multi-Status";
+		case 208: return "Already Reported";
+		case 226: return "IM Used";
 
-	case 300: return "Multiple Choices";
-	case 301: return "Moved Permanently";
-	case 302: return "Found";
-	case 303: return "See Other";
-	case 304: return "Not Modified";
-	case 305: return "Use Proxy";
-	case 307: return "Temporary Redirect";
-	case 308: return "Permanent Redirect";
+		case 300: return "Multiple Choices";
+		case 301: return "Moved Permanently";
+		case 302: return "Found";
+		case 303: return "See Other";
+		case 304: return "Not Modified";
+		case 305: return "Use Proxy";
+		case 307: return "Temporary Redirect";
+		case 308: return "Permanent Redirect";
 
-	case 400: return "Bad Request";
-	case 401: return "Unauthorized";
-	case 402: return "Payment Required";
-	case 403: return "Forbidden";
-	case 404: return "Not Found";
-	case 405: return "Method Not Allowed";
-	case 406: return "Not Acceptable";
-	case 407: return "Proxy Authentication Required";
-	case 408: return "Request Timeout";
-	case 409: return "Conflict";
-	case 410: return "Gone";
-	case 411: return "Length Required";
-	case 412: return "Precondition Failed";
-	case 413: return "Payload Too Large";
-	case 414: return "URI Too Long";
-	case 415: return "Unsupported Media Type";
-	case 416: return "Range Not Satisfiable";
-	case 417: return "Expectation Failed";
-	case 418: return "I'm a teapot";
-	case 422: return "Unprocessable Entity";
-	case 423: return "Locked";
-	case 424: return "Failed Dependency";
-	case 426: return "Upgrade Required";
-	case 428: return "Precondition Required";
-	case 429: return "Too Many Requests";
-	case 431: return "Request Header Fields Too Large";
-	case 451: return "Unavailable For Legal Reasons";
+		case 400: return "Bad Request";
+		case 401: return "Unauthorized";
+		case 402: return "Payment Required";
+		case 403: return "Forbidden";
+		case 404: return "Not Found";
+		case 405: return "Method Not Allowed";
+		case 406: return "Not Acceptable";
+		case 407: return "Proxy Authentication Required";
+		case 408: return "Request Timeout";
+		case 409: return "Conflict";
+		case 410: return "Gone";
+		case 411: return "Length Required";
+		case 412: return "Precondition Failed";
+		case 413: return "Payload Too Large";
+		case 414: return "URI Too Long";
+		case 415: return "Unsupported Media Type";
+		case 416: return "Range Not Satisfiable";
+		case 417: return "Expectation Failed";
+		case 418: return "I'm a teapot";
+		case 422: return "Unprocessable Entity";
+		case 423: return "Locked";
+		case 424: return "Failed Dependency";
+		case 426: return "Upgrade Required";
+		case 428: return "Precondition Required";
+		case 429: return "Too Many Requests";
+		case 431: return "Request Header Fields Too Large";
+		case 451: return "Unavailable For Legal Reasons";
 
-	case 500: return "Internal Server Error";
-	case 501: return "Not Implemented";
-	case 502: return "Bad Gateway";
-	case 503: return "Service Unavailable";
-	case 504: return "Gateway Time-out";
-	case 505: return "HTTP Version Not Supported";
-	case 506: return "Variant Also Negotiates";
-	case 507: return "Insufficient Storage";
-	case 508: return "Loop Detected";
-	case 510: return "Not Extended";
-	case 511: return "Network Authentication Required";
+		case 500: return "Internal Server Error";
+		case 501: return "Not Implemented";
+		case 502: return "Bad Gateway";
+		case 503: return "Service Unavailable";
+		case 504: return "Gateway Time-out";
+		case 505: return "HTTP Version Not Supported";
+		case 506: return "Variant Also Negotiates";
+		case 507: return "Insufficient Storage";
+		case 508: return "Loop Detected";
+		case 510: return "Not Extended";
+		case 511: return "Network Authentication Required";
 
-	default: return 0;
+		default: return 0;
 	}
 }
 
@@ -92,7 +91,7 @@ static const WEBHEADER defaultHeaders[] = {
 	{NULL, NULL}
 };
 
-static int sendBuffer(WEBCLIENT* wcl, int len) {
+static int sendBuffer(WEBCLIENT* wcl, uint32_t len) {
 	return send(wcl->sock, wcl->buffer, len, 0);
 }
 
@@ -193,13 +192,9 @@ static void GenerateResponse(WEBCLIENT* wcl) {
 }
 
 static void freeWsClient(WEBCLIENT* wcl) {
-	if(wcl->request)
-		Memory_Free((void*)wcl->request);
-	if(wcl->wsKey)
-		Memory_Free((void*)wcl->wsKey);
-	if(wcl->wsFrame)
-		WebSocket_DestroyFrame(wcl->wsFrame);
-
+	if(wcl->request) Memory_Free((void*)wcl->request);
+	if(wcl->wsKey) Memory_Free((void*)wcl->wsKey);
+	if(wcl->wsFrame) WebSocket_DestroyFrame(wcl->wsFrame);
 	Memory_Free(wcl);
 }
 
@@ -218,8 +213,7 @@ static char* ReadSockUntil(WEBCLIENT* wcl, size_t len, char sym) {
 			len -= ret;
 		else
 			break;
-	}
-	while(len > 0 && *tmp != sym && (*tmp == '\r' ? tmp : tmp++));
+	} while(len > 0 && *tmp != sym && (*tmp == '\r' ? tmp : tmp++));
 
 	*tmp = '\0';
 	return wcl->buffer;
@@ -252,10 +246,14 @@ static void ReadHeader(WEBCLIENT* wcl, const char* value) {
 	}
 }
 
+static void sendFrame(WEBCLIENT* wcl, const char* data, uint32_t dlen, char opcode) {
+	uint32_t len = WebSocket_Encode(wcl->buffer, 1024, data, dlen, opcode);
+	if(len) sendBuffer(wcl, len);
+}
+
 static bool HandleWebSocketFrame(WEBCLIENT* wcl) {
 	WSFRAME* ws = wcl->wsFrame;
-	if(ws->opcode == 0x08)
-		return true;
+	if(ws->opcode == 0x08) return true;
 
 	return false;
 }
@@ -297,6 +295,14 @@ static void HandleGetRequest(WEBCLIENT* wcl, char* buffer) {
 		wcl->wsFrame = (WSFRAME*)Memory_Alloc(1, sizeof(WSFRAME));
 		WebSocket_Setup(wcl->wsFrame, wcl->sock);
 		wcl->respCode = 101;
+		GenerateResponse(wcl);
+
+		while(1) {
+			if(WebSocket_ReceiveFrame(wcl->wsFrame)) {
+				if(!HandleWebSocketFrame(wcl)) continue;
+			}
+			break;
+		}
 	} else {
 		const char* filename = "index.html";
 		if(!String_CaselessCompare(wcl->request, "/")) {
@@ -304,19 +310,8 @@ static void HandleGetRequest(WEBCLIENT* wcl, char* buffer) {
 		}
 		if(!SendZippedFile(wcl, filename)) {
 			wclSetError(wcl, 404, "Specified file not found in " CPL_ZIP);
+			GenerateResponse(wcl);
 		}
-	}
-
-	if(wcl->wsUpgrade) {
-		while(1) {
-			if(WebSocket_ReceiveFrame(wcl->wsFrame)) {
-				if(!HandleWebSocketFrame(wcl))
-					continue;
-			}
-			break;
-		}
-	} else {
-		GenerateResponse(wcl);
 	}
 }
 
