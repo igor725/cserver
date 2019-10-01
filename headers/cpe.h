4a94d67fd5f86b69ced91bdd09f1e533115b88ff
igor725 <admin@igvx.ru>
2019-10-01 13:18:30 +0300

    Warning fix

--- headers/cpe.h
+++ headers/cpe.h
@@ -12,9 +12,8 @@ struct extReg {
 };
 
 API bool CPE_CheckModel(const char* model);
-
-void CPEPacket_WriteInfo(CLIENT cl);
 void CPE_StartHandshake(CLIENT client);
+
 bool CPEHandler_ExtInfo(CLIENT client, char* data);
 bool CPEHandler_ExtEntry(CLIENT client, char* data);
 bool CPEHandler_TwoWayPing(CLIENT client, char* data);
@@ -23,9 +22,11 @@ bool CPEHandler_PlayerClick(CLIENT client, char* data);
 void CPEPacket_WriteInfo(CLIENT client);
 void CPEPacket_WriteExtEntry(CLIENT client, EXT* ext);
 void CPEPacket_WriteClickDistance(CLIENT client, short dist);
-void CPEPacket_WriteWeatherType(CLIENT client, Weather type);
-void CPEPacket_WriteSetHotBar(CLIENT client, Order order, BlockID block);
-void CPEPacket_WriteSetModel(CLIENT client, ClientID id, const char* model);
 void CPEPacket_WriteInventoryOrder(CLIENT client, Order order, BlockID block);
 void CPEPacket_WriteHoldThis(CLIENT client, BlockID block, bool preventChange);
+void CPEPacket_WriteSetHotBar(CLIENT client, Order order, BlockID block);
+void CPEPacket_WriteWeatherType(CLIENT client, Weather type);
+void CPEPacket_WriteTwoWayPing(CLIENT client, uint8_t direction, short num);
+void CPEPacket_WriteSetModel(CLIENT client, ClientID id, const char* model);
+void CPEPacket_WriteBlockPerm(CLIENT client, BlockID id, bool allowPlace, bool allowDestroy);
 #endif
