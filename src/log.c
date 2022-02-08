627d9081d4c947f8a9109bcf2de9f23cb973e2e2
igor725 <gvaldovigor@gmail.com>
2022-02-08 03:29:51 +0300

    Update log.c

--- src/log.c
+++ src/log.c
@@ -72,18 +72,19 @@ void Log_Print(cs_byte flag, cs_str str, va_list *args) {
 		)) > 0)
 			buffer.offset += ret;
 
-		if(args)
+		if(args) {
 			if((ret = String_FormatBufVararg(
 				buffer.data + buffer.offset,
 				LOG_BUFSIZE - buffer.offset - 3, str, args
 			)) > 0)
 				buffer.offset += ret;
-		else
+		} else {
 			if((ret = (cs_int32)String_Append(
 				buffer.data + buffer.offset,
 				LOG_BUFSIZE - buffer.offset - 3, str
 			)) > 0)
 				buffer.offset += ret;
+		}
 
 		buffer.data[buffer.offset++] = '\r';
 		buffer.data[buffer.offset++] = '\n';
