46a0b3ef2a10daa3a8dd021531b5e449da0e5974
igor725 <gvaldovigor@gmail.com>
2022-02-13 00:58:32 +0300

    Define Plugin_DeclareInterfaces added

--- src/core.h
+++ src/core.h
@@ -169,6 +169,8 @@ EXP cs_bool Plugin_Load(void);
 EXP cs_bool Plugin_Unload(cs_bool force);
 EXP void Plugin_RecvInterface(cs_str name, void *ptr, cs_size size);
 EXP cs_int32 Plugin_ApiVer, Plugin_Version;
+#define Plugin_DeclareInterfaces(_N) EXP PluginInterface Plugin_Interfaces[_N]; \
+PluginInterface Plugin_Interfaces[_N] = 
 #define Plugin_SetVersion(ver) cs_int32 Plugin_ApiVer = PLUGIN_API_NUM, Plugin_Version = ver;
 #define PLUGIN_IFACE_DONE {NULL, NULL, 0}
 #define PLUGIN_IFACE_ADD(n, i) {n, &(i), sizeof(i)},
