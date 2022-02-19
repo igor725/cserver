2148880f6896ab2ade656121d7a2a2cad7a8c8c3
igor725 <gvaldovigor@gmail.com>
2022-02-19 22:36:23 +0300

    Vec_IsInvalid replaced with Vec_IsNegative

--- src/vector.h
+++ src/vector.h
@@ -34,7 +34,7 @@ static inline cs_bool Ang_Compare(const Ang *a, const Ang *b) {
 #define Vec_Copy(dv, sv) (dv).x = (cs_float)(sv).x, (dv).y = (cs_float)(sv).y, (dv).z = (cs_float)(sv).z
 #define SVec_Copy(dv, sv) (dv).x = (cs_int16)(sv).x, (dv).y = (cs_int16)(sv).y, (dv).z = (cs_int16)(sv).z
 #define Vec_Set(v, vx, vy, vz) (v).x = vx, (v).y = vy, (v).z = vz
-#define Vec_IsInvalid(v) ((v)->x == -1 && (v)->y == -1 && (v)->z == -1)
+#define Vec_IsNegative(v) ((v).x < 0 || (v).y < 0 || (v).z < 0)
 #define Vec_IsZero(v) ((v).x == 0 && (v).y == 0 && (v).z == 0)
 #define Vec_HaveZero(v) ((v).x == 0 || (v).y == 0 || (v).z == 0)
 #define Vec_Scale(v, s) (v).x *= s, (v).y *= s, (v).z *= s
