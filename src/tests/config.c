24aab5bdf6c59ee0233ced1314bdb463f18758d5
igor725 <gvaldovigor@gmail.com>
2022-06-08 09:09:47 +0300

    Whoops

--- src/tests/config.c
+++ src/tests/config.c
@@ -6,7 +6,7 @@ cs_bool Tests_Config(void) {
 	Tests_NewTask("Create config store");
 	CStore *store;
 	CEntry *ent;
-	Tests_Assert((store = Config_NewStore("__test.cfg")) != NULL, "create config store");
+	Tests_Assert((store = Config_NewStore("__test")) != NULL, "create config store");
 	Tests_Assert((ent = Config_NewEntry(store, "test-key", CONFIG_TYPE_BOOL)) != NULL, "create boolean entry");
 	Tests_Assert(Config_TypeNameToEnum(Config_TypeName(ent->type)) == ent->type, "");
 	Config_SetComment(ent, "_test_CoMment1");
